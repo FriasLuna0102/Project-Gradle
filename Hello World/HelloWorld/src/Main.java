1a0d74283b457b56d0321b79d98d75e23c14350f
Frias Luna <starlinluna0102@gmail.com>
2024-01-10 20:28:45 -0400

    l

---
+++ Hello World/HelloWorld/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+       int x=10, y =5;
+
+    for(int i = 0; i<x; i++){
+
+        if(i<y) {
+            System.out.println("Hello, el valor es de:" + i);
+        }else{
+            System.out.println("Hello, el valor es de:" + y);
+        }
+    }
+
+    }
+}

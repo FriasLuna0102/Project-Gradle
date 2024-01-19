065a7d11c74b904432279e54be219408c275ec87
Frias Luna <starlinluna0102@gmail.com>
2024-01-19 12:06:50 -0400

    Project Gradle

--- Hola-mundo-1850/src/main/java/org/example/Main.java
+++ Hola-mundo-1850/src/main/java/org/example/Main.java
@@ -4,8 +4,9 @@ import io.javalin.Javalin;
 
 public class Main {
     public static void main(String[] args) {
-        var app = Javalin.create(/*config*/)
+        var app = Javalin.create()
                 .get("/", ctx -> ctx.result("Hello World"))
                 .start(7070);
     }
+
 }
